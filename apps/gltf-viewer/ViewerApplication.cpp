320497ffff51fb8d5adaa281a6cef00d0034a183
Amel BEN HAMOUDA <amel@debian>
2020-04-09 13:28:43 +0200

    debut rapport

--- apps/gltf-viewer/ViewerApplication.cpp
+++ apps/gltf-viewer/ViewerApplication.cpp
@@ -17,9 +17,6 @@
 
 #include "Cube.hpp"
 
-#define __STDC_WANT_LIB_EXT1__ 1
-#include <string.h> 
-
 void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
         glfwSetWindowShouldClose(window, 1);
